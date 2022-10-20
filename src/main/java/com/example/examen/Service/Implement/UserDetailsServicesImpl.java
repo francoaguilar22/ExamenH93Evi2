4736f98a174df80cf7d484b59709a22a8152cce9
John Doe <jflorese@certus.edu.pe>
2022-10-19 20:43:11 -0500

    1

--- src/main/java/com/example/examen/Service/Implement/UserDetailsServicesImpl.java
+++ src/main/java/com/example/examen/Service/Implement/UserDetailsServicesImpl.java
@@ -16,7 +16,7 @@ public class UserDetailsServicesImpl implements UserDetailsService {
     private UsuarioRepository userepo;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String ) throws UsernameNotFoundException {
         UsuariosEntity userload = userepo.findByUsername(username);
         if (userload ==null){
             throw new UsernameNotFoundException ("Usuario No encontrado");
--- src/main/java/com/example/examen/repository/matriculaRepository.java
+++ src/main/java/com/example/examen/repository/matriculaRepository.java
@@ -3,5 +3,5 @@ package com.example.examen.repository;
 import com.example.examen.Bean.matriculaBean;
 import org.springframework.data.repository.CrudRepository;
 
-public interface matriculaRepository extends CrudRepository <matriculaBean,Integer> {
+public interface matriculaRepository extends CrudRepository <matriculaBean,> {
 }
