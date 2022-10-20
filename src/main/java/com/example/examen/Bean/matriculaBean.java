f8f9c5050792ff6a28cabff80bc52ac1ddb71ac5
John Doe <jflorese@certus.edu.pe>
2022-10-19 21:03:20 -0500

    1

--- src/main/java/com/example/examen/Bean/matriculaBean.java
+++ src/main/java/com/example/examen/Bean/matriculaBean.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class matriculaBean {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.)
     private Integer idmatricula;
 
     private Integer idestudiante;
--- src/main/java/com/example/examen/Config/AutenticacionPoint.java
+++ src/main/java/com/example/examen/Config/AutenticacionPoint.java
@@ -14,6 +14,6 @@ public class AutenticacionPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse., "Token no Autorizado");
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token no Autorizado");
     }
 }
